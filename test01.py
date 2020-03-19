e1083823bf7cfb38ae13d5dcc22d3d51bdca27fc
Bonnie <bonnie.chiang@kkday.com>
2020-03-19 14:52:41 +0800

    test01.py

--- test01.py
+++ test01.py
@@ -4,6 +4,10 @@ class Config:
     def _init_(self):
         self.config = configparser.ConfigParser()
         self.config.read('Config.ini')
+        
+class Print:
+    def _init_(self):
+        self.config = Config()        
         section_a_Value = self.config.get('Key_DEF')
         section_b_Value = self.config.get('Alarm')
 
